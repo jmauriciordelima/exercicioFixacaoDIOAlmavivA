dc0846baffda1a5c10cb5566f4d2c05e074e0095
José Maurício <mauricio.rodriguesdelima@gmail.com>
2026-04-08 21:48:32 -0300

    Exercícios de Fixação - exercício de controle 1 - Validações de entrada e mensagens para o usuário.

--- src/exerciciosControleRepeticao/ExercicioATeste.java
+++ src/exerciciosControleRepeticao/ExercicioATeste.java
@@ -11,23 +11,26 @@ public class ExercicioATeste {
         ExercicioATabuada tabuada = new ExercicioATabuada();
 
         int numeroUsuario = -1;
+        boolean continuar = true;
 
-        System.out.println("Para finalizar o programa, basta digitar [0].");
 
         do {
+
+            System.out.println("\nPara finalizar o programa, basta digitar [0].\n");
+
             try {
                 System.out.print("Informe qual tabuada deseja verificar -> ");
                 numeroUsuario = entrada.nextInt();
                 if (numeroUsuario == 0) {
                     System.out.println("Finalizando... até logo!");
-                    return;
+                    continuar = false;
                 }
                 tabuada.gerarTabuada(numeroUsuario);
             } catch (InputMismatchException e) {
                 System.out.println("Caractere não identificado, digite um número válido.");
                 entrada.next();
             }
-        } while (true);
+        } while (continuar);
 
     }
 }
