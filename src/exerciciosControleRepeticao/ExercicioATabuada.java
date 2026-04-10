919b0f46eb51c6999a7b4be7a14b2d7696326241
José Maurício <mauricio.rodriguesdelima@gmail.com>
2026-04-10 02:04:12 -0300

    Exercícios de Fixação - exercício de controle 1 - Validações de entrada e mensagens para o usuário - refinamento.

--- src/exerciciosControleRepeticao/ExercicioATabuada.java
+++ src/exerciciosControleRepeticao/ExercicioATabuada.java
@@ -2,28 +2,39 @@ package exerciciosControleRepeticao;
 
 public class ExercicioATabuada {
 
+    private static final int TABUADA_MINIMA = 1;
+    private static final int TABUADA_MAXIMA = 1000;
+    private static final int TABUADA_ATE = 10;
+
     public void gerarTabuada(int numero) {
 
-        if (validaEntrada(numero) == 0) {
-            return;
-        }
+        numero = validarEntrada(numero);
 
         System.out.printf("Tabuada do [%d]%n", numero);
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = TABUADA_MINIMA; i <= TABUADA_ATE; i++) {
             System.out.printf("%d X %d = %d%n", numero, i, (i * numero));
+
         }
 
     }
 
-    private int validaEntrada(int numero) {
+    private int validarEntrada(int numero) {
+
 
         if (numero < 0) {
-            System.out.println("Não existe tabuada negativa. Tente novamente!");
-            return 0;
+            System.out.println("Não existe tabuada negativa. Convertendo para positivo.");
+            numero = Math.abs(numero);
+        }
+
+        if (numero > TABUADA_MAXIMA) {
+            System.out.println("O valor máximo para verificar a tabuada é 1000.\n" +
+                    "O valor será convertido para o valor máximo permitido.");
+            return TABUADA_MAXIMA;
         }
 
         return numero;
     }
+
 
 }
--- src/exerciciosControleRepeticao/ExercicioATeste.java
+++ src/exerciciosControleRepeticao/ExercicioATeste.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class ExercicioATeste {
 
+    private static final int OPCAO_SAIR = 0;
+
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
@@ -13,19 +15,22 @@ public class ExercicioATeste {
         int numeroUsuario = -1;
         boolean continuar = true;
 
-
         do {
 
             System.out.println("\nPara finalizar o programa, basta digitar [0].\n");
 
             try {
+
                 System.out.print("Informe qual tabuada deseja verificar -> ");
                 numeroUsuario = entrada.nextInt();
-                if (numeroUsuario == 0) {
-                    System.out.println("Finalizando... até logo!");
+
+                if (numeroUsuario == OPCAO_SAIR) {
                     continuar = false;
+                    System.out.println("Finalizando... até logo!");
+                } else {
+                    tabuada.gerarTabuada(numeroUsuario);
                 }
-                tabuada.gerarTabuada(numeroUsuario);
+
             } catch (InputMismatchException e) {
                 System.out.println("Caractere não identificado, digite um número válido.");
                 entrada.next();
